26f6604144928a25dcc207dca7359136f742109c
Esteban Sayago <sayagoo823@gmail.com>
2023-08-01 14:20:40 -0300

    agregar validador unico

--- backend/models.js
+++ backend/models.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const url = process.env.MONGODB_URI
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 console.log('conectando a la base de datos...')
 mongoose.connect(url).then(() => console.log('conexion exitosa'))
@@ -9,14 +10,18 @@ mongoose.connect(url).then(() => console.log('conexion exitosa'))
 const personaSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
-    required: true,
+    minLength: [3, 'el nombre es demasiado corto, minimo 3 caracteres'],
+    required: [true, 'El nombre no esta definido'],
+    unique: [true, 'El nombre debe ser unico'],
   },
   number: {
     type: Number,
-    required: true,
+    required: [true, 'el numero no esta definido'],
+    min: [10000000, 'el numero debe tener al menos 8 digitos'],
   },
 })
+
+personaSchema.plugin(uniqueValidator)
 
 personaSchema.set('toJSON', {
   transform: (doc, obj) => {
